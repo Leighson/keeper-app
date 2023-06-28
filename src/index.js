4bca00e13b4deeaab8cae2a524948e5c593acdf6
Lee Santos <lee.m.santos@outlook.com>
2023-06-28 16:08:41 -0400

    Build components for footer, header, and notes function.

--- src/index.js
+++ src/index.js
@@ -9,4 +9,13 @@
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//HINT: You will need to study the classes in the styles.css file to apply styling.
+
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+
+ReactDom.render(
+    <App />,
+    document.getElementById("root")
+);
