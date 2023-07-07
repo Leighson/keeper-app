59aca6324418f9a32a441ca17839a8f7ccf7523b
Lee Santos <lee.m.santos@outlook.com>
2023-07-07 18:49:49 -0400

    Formatting.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,19 +5,15 @@ import Footer from "./Footer";
 import notes from "../notes";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            {notes.map(note => (
-                <Note
-                    key={note.id}
-                    title={note.title}
-                    content={note.content}
-                />
-            ))}
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ const date = new Date();
 const currentYear = date.getFullYear();
 
 function Footer() {
-    return (
-        <footer>
-            <p>Copyright {currentYear}</p>
-        </footer>
-    )
-};
+  return (
+    <footer>
+      <p>Copyright {currentYear}</p>
+    </footer>
+  );
+}
 
 export default Footer;
