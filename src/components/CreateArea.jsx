06fe8b5611af7fdb8007c6e5405614f95bfefe9b
Lee Santos <lee.m.santos@outlook.com>
2023-07-16 22:41:46 -0400

    Store note title and content. Save into temp db.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [noteDatabase, setNoteDatabase] = useState([]);
+
+  function handleChange(event) {
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setNote({
+      title: inputName === "title" ? inputValue : note.title,
+      content: inputName === "content" ? inputValue : note.content,
+    });
+
+    console.log(note);
+  }
+
+  function addNote(event) {
+    setNoteDatabase({
+      title: note.title,
+      content: note.content,
+    });
+
+    setNote({
+      title: "",
+      content: ""
+    });
+
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={addNote}>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleChange}
+          value={note.content}
+        />
+        <button type="submit" onClick={addNote}>
+          Add
+        </button>
       </form>
     </div>
   );
